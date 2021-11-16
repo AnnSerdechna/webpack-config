46f3fd67d6a753fcf1be7c253aeb95a456527d1f
Anna <serdechnaann@gmail.com>
2021-11-16 09:32:56 +0200

    Add image support

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import "../styles/index.scss";
 import Recipes from "./Recipes";
+import image from "../images/img3.jpg"
 
 function App() {
   return (
     <>
-      <h1 className="header">Oh hai, React!</h1>
+      <img src={image} alt="img" />
+      <section className="hero"></section>
       <Recipes />
     </>
   );
